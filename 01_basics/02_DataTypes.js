afa450c3b188d6bd47a39658b3afb26b0433ecd5
ShlokPauriyal <shlokpauriyal@gmail.com>
2026-08-07 07:10:01 +0000

    DataTypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,6 @@
+"use strict" // treat all JS code as newer version
+
+// alert(2+3); // we are using nodejs, not browser
+
+console.log(typeof null); // null datatype is OBJEECT
+console.log(typeof undefined); // datatype -> undefined
