f2780b8446b667f55a2940e3a3f8634d1894c950
ShlokPauriyal <shlokpauriyal@gmail.com>
2026-08-07 09:26:54 +0000

    DataTypes Coversions

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,21 @@
+let First_Name = "shlok";
+let Roll_No = 18;
+let Eligible = 1;
+
+// console.log(typeof(First_Name));
+
+// let Is_FirstName = Number(First_Name);
+// console.log(Is_FirstName);          // NaN -> Not a number
+// console.log(typeof Is_FirstName);   // converted into number datatype
+
+
+// console.log(typeof Roll_No);
+// let Check_Number = String(Roll_No);
+// console.log(Check_Number);
+// console.log(typeof Check_Number);  
+
+
+console.log(typeof Eligible);
+let Check_Eligibility = Boolean(Roll_No);
+console.log(Check_Eligibility);
+console.log(typeof Check_Eligibility);
