0ffab96f4626149d286abf527bc64ce6eda6b7d3
ShlokPauriyal <shlokpauriyal@gmail.com>
2026-08-07 10:00:31 +0000

    Added Operations

--- 01_basics/03_ConversionOperations.js
+++ 01_basics/03_ConversionOperations.js
@@ -2,6 +2,10 @@ let First_Name = "shlok";
 let Roll_No = 18;
 let Eligible = 1;
 
+// console.log(+"");
+// console.log(+true);
+
+
 // console.log(typeof(First_Name));
 
 // let Is_FirstName = Number(First_Name);
@@ -15,7 +19,19 @@ let Eligible = 1;
 // console.log(typeof Check_Number);  
 
 
-console.log(typeof Eligible);
-let Check_Eligibility = Boolean(Roll_No);
-console.log(Check_Eligibility);
-console.log(typeof Check_Eligibility);
+// console.log(typeof Eligible);
+// let Check_Eligibility = Boolean(Roll_No);
+// console.log(Check_Eligibility);
+// console.log(typeof Check_Eligibility);
+
+// **************************************OPERATIONS*************************************************
+
+// console.log("1" + 2 + 2);
+// console.log(2 + 3 + "1");
+
+let a = 1; 
+console.log(a);
+let b = a++;
+
+console.log(a);
+console.log(b);
