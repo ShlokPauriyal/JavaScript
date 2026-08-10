f76baadf39cb473dc09c0d544bee70be554286b0
ShlokPauriyal <shlokpauriyal@gmail.com>
2026-08-10 10:06:59 +0000

    Comparisions

---
+++ 01_basics/04_Comparisions.js
@@ -0,0 +1,13 @@
+// console.log("2" > 1); //convert String "2" -> into Number 2, and further compare it 
+// console.log("02" > 1);
+
+// console.log(null > 0);  // Comparision convert null into 0 and then compare it
+// console.log(null == 0); // in equality NULL work differently as compare to Comparision 
+// console.log(null >= 0);  // Comparision convert null into 0 and then compare it 
+
+// console.log(true == 0);
+// console.log(false == 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
