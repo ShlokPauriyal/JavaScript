e2e561247f87fc2bb0e22dfc8106a2c7439663aa
ShlokPauriyal <shlokpauriyal@gmail.com>
2026-09-03 08:54:35 +0000

    Date and its methods

---
+++ 01_basics/05_Date_In_JS.js
@@ -0,0 +1,9 @@
+// let myDate = new Date();
+// console.log(myDate.toDateString());
+// console.log(myDate.getMilliseconds());
+// console.log(typeof myDate);
+// console.log(myDate.toLocaleTimeString());
+
+// let createdDate = new Date(2026,0,1); // MONTH START FROM 0 -> JAN TO 11 -> DEC IN JS
+// console.log(createdDate.toDateString());
+
